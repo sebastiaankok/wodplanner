e3ad5dd1f4b3970219575a55ee8786edee1ba6d6
Sebastiaan Kok <sebastiaan@linux.com>
2026-04-18 00:42:52 +0200

    chore: simple deploy script for development

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+IMAGE="ghcr.io/sebastiaankok/wodplanner"
+TAG="${1:-main}"
+
+docker build --platform linux/amd64 -t "${IMAGE}:${TAG}" .
+docker push "${IMAGE}:${TAG}"
+
+kubectl --kubeconfig ~/.kube/configs/k3s.yaml --context k3s-home \
+  delete po -n wodplanner --all
